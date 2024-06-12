9e6bb6c3bfbe31494b2bf62982ec37776309dc11
Chinonso Mba <142128065+Chinonsomba@users.noreply.github.com>
2024-06-12 09:25:32 +0100

    Add files via upload

---
+++ Ab_testing.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+# Load the data
+data = pd.read_csv(r'C:\Users\USER\OneDrive\Desktop\A_B(Testing)\cookie_cats.csv')
+
+# Inspect the data to ensure all columns are correctly read
+print(data.info())
+print(data.head(10))
+
+
+
+
+
+
+
+
+
+
